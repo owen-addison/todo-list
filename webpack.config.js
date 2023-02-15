f037e75ba0123856360963cb469e3bce22206a1d
Owen Lewis <owenalewis@gmail.com>
2023-02-15 13:51:58 +0000

    Removes HMTL webpack plugin and rebuilds

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -7,14 +6,8 @@ module.exports = {
     index: "./src/index.js",
   },
   devtool: "inline-source-map",
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Output Management",
-    }),
-  ],
   output: {
     filename: "[name].bundle.js",
     path: path.resolve(__dirname, "dist"),
-    clean: true,
   },
 };
