f5aa338f177c848a6268aa1415ef896681a4812f
Owen Lewis <owenalewis@gmail.com>
2023-02-15 17:27:38 +0000

    Adds module for returning a todo item

---
+++ src/todoItem.js
@@ -0,0 +1,35 @@
+/* 
+*********************************************************************
+
+Factory function to return a todoItem object
+OAL 15-02-2023
+Version 0.1 genesis
+
+This function called by project or todoList code to create and return a todoItem
+
+********************************************************************* 
+*/
+
+const todoItem = (
+  name = "Todo Item",
+  info = "",
+  dueDate = null,
+  priority = "none",
+  checklist = [],
+  status = "pending"
+) => {
+  const state = {
+    name,
+    info,
+    creationDate: new Date(),
+    dueDate,
+    priority,
+    checklist,
+    status,
+  };
+  return {
+    state,
+  };
+};
+
+export default todoItem;
