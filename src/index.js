61e0c679c2478dc656964b9b298fd05d95e95e31
Owen Lewis <owenalewis@gmail.com>
2023-02-15 17:28:42 +0000

    Imports modules for todoItem and todoList

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+/* 
+*********************************************************************
+
+Factory function to return a ToDo object
+OAL 15-02-2023
+Version 0.1 genesis
+
+This function called by project code to create and return a ToDo
+
+********************************************************************* 
+*/
+
+import todoItem from "./todoItem";
+import todoList from "./todoList";
+
+const myTodo = todoItem();
+const myTodoList = todoList();
+
+console.table(myTodoList);
