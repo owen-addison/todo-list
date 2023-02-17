2ec413235e4305141fc00d059a01bbc937f8c4c4
Owen Lewis <owenalewis@gmail.com>
2023-02-17 14:10:02 +0000

    Adds creator() and remover(), adds id and type props

--- src/todoProto.js
+++ src/todoProto.js
@@ -1,11 +1,28 @@
-const todoProto = (name = null, info = null) => {
+const getDefaultName = (arrayLength) => `Item ${arrayLength + 1}`;
+
+const creator = (state, array, object) => ({
+  create: (
+    objName = getDefaultName(state[`${array}`].length),
+    idNum = state[`${array}`].length + 1
+  ) => {
+    state[`${array}`].push(object(objName, idNum));
+  },
+});
+
+const remover = (state, array) => ({
+  deleteItem: (id) => state[`${array}`].filter((element) => element.id !== id),
+});
+
+const todoProto = (name, id, info, type) => {
   const state = {
     name,
+    id,
     info,
+    type,
   };
   return {
     ...state,
   };
 };
 
-export default todoProto;
+export { todoProto, creator, remover };
