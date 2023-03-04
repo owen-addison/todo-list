eb0f5b1e5ddb68a81c0c1848e501f5836c658729
Owen Lewis <owenalewis@gmail.com>
2023-03-04 07:21:00 +0000

    Adds info property to create in creator

--- src/todoProto.js
+++ src/todoProto.js
@@ -3,10 +3,11 @@ const getDefaultName = (arrayLength) => `Item ${arrayLength + 1}`;
 const creator = (state, array, object) => ({
   create: (
     objName = getDefaultName(state[`${array}`].length),
-    id = `${state.id}_td${state[`${array}`].length + 1}`
+    id = `${state.id}_td${state[`${array}`].length + 1}`,
+    info = undefined
     // id = state[`${array}`].length + 1,
   ) => {
-    state[`${array}`].push(object(objName, id));
+    state[`${array}`].push(object(objName, id, info));
   },
 });
 
