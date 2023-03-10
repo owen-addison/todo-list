7cd32dfdb5494805f216b8c1215f3dc04424df4f
Owen Lewis <owenalewis@gmail.com>
2023-03-10 11:28:26 +0000

    Changes initialiser.js to domManager.js, refactors code from index.js

---
+++ src/domManager.js
@@ -0,0 +1,229 @@
+import plusIcon from "./images/plus-box-outline.svg";
+import trashCanIcon from "./images/trash-can-outline.svg";
+import fileEditIcon from "./images/file-edit-outline.svg";
+
+/*
+-------------------------
+DOM SETUP
+-------------------------
+*/
+const headerTitleElement = "h2";
+const listTitleElement = "h3";
+const todoNameElement = "h5";
+
+function setUpDOM() {
+  // Get content div element
+  const content = document.querySelector(".content");
+
+  // Create content container
+  const contentContainer = document.createElement("div");
+  contentContainer.classList.add("content-container");
+
+  // Create sidebar section
+  const sidebar = document.createElement("div");
+  sidebar.classList.add("sidebar");
+
+  // Create project view
+  const projectView = document.createElement("div");
+  projectView.classList.add("project-view");
+
+  // Create project view header
+  const projectHeader = document.createElement("div");
+  projectHeader.classList.add("project-header");
+  // Add title to project header
+  const headerTitle = document.createElement(headerTitleElement);
+  projectHeader.appendChild(headerTitle);
+  // Add icon container to header
+  const headerIconContainer = document.createElement("div");
+  headerIconContainer.classList.add("header-icon-container");
+  projectHeader.appendChild(headerIconContainer);
+  // Add icons to container
+  const headerAddIcon = new Image();
+  headerAddIcon.src = plusIcon;
+  headerAddIcon.classList.add("add-icon");
+  // headerAddIcon.setAttribute("projId", "proj1");
+  headerAddIcon.setAttribute("type", "proj");
+  headerIconContainer.appendChild(headerAddIcon);
+  const headerEditIcon = new Image();
+  headerEditIcon.src = fileEditIcon;
+  headerEditIcon.classList.add("edit-icon");
+  // headerEditIcon.setAttribute("projId", "proj");
+  headerEditIcon.setAttribute("type", "proj");
+  headerIconContainer.appendChild(headerEditIcon);
+  const headerRemoveIcon = new Image();
+  headerRemoveIcon.src = trashCanIcon;
+  headerRemoveIcon.classList.add("del-icon");
+  headerRemoveIcon.setAttribute("type", "proj");
+  headerIconContainer.appendChild(headerRemoveIcon);
+
+  // Add project header to project view
+  projectView.appendChild(projectHeader);
+
+  // Create project view main section
+  const projectContainer = document.createElement("div");
+  projectContainer.classList.add("project-container");
+
+  // Add project container to project view
+  projectView.appendChild(projectContainer);
+
+  // Add elements to document
+  contentContainer.appendChild(sidebar);
+  contentContainer.appendChild(projectView);
+  content.appendChild(contentContainer);
+}
+
+/*
+-------------------------
+DOM UPDATE
+-------------------------
+*/
+// Function to display todo list
+const displayList = (list) => {
+  // Get project container
+  const projectContainer = document.querySelector(".project-container");
+
+  // Create new element
+  const listContainer = document.createElement("div");
+  // Assign class and id name
+  listContainer.classList.add("list-container");
+  const listIdName = `${list.id}`;
+  listContainer.setAttribute("id", listIdName);
+
+  // Display name as title
+  const titleContainer = document.createElement("div");
+  titleContainer.classList.add("title-container");
+  listContainer.appendChild(titleContainer);
+  const title = document.createElement(listTitleElement);
+  title.classList.add("list-title");
+  title.textContent = list.name;
+  titleContainer.appendChild(title);
+
+  // Display icons
+  const iconContainer = document.createElement("div");
+  iconContainer.classList.add("icon-container");
+  const addIcon = new Image();
+  addIcon.src = plusIcon;
+  addIcon.classList.add("add-icon");
+  addIcon.setAttribute("id", `add-${listIdName}`);
+  addIcon.setAttribute("type", "list");
+  addIcon.setAttribute("listId", listIdName);
+  iconContainer.appendChild(addIcon);
+  const editIcon = new Image();
+  editIcon.src = fileEditIcon;
+  editIcon.classList.add("edit-icon");
+  editIcon.setAttribute("id", `edit-${listIdName}`);
+  editIcon.setAttribute("type", "list");
+  editIcon.setAttribute("listId", listIdName);
+  iconContainer.appendChild(editIcon);
+  const removeIcon = new Image();
+  removeIcon.src = trashCanIcon;
+  removeIcon.classList.add("del-icon");
+  removeIcon.setAttribute("id", `del-${listIdName}`);
+  removeIcon.setAttribute("type", "list");
+  removeIcon.setAttribute("listId", listIdName);
+  iconContainer.appendChild(removeIcon);
+  titleContainer.appendChild(iconContainer);
+
+  // Display list contents
+  const todoContainer = document.createElement("div");
+  todoContainer.classList.add("todo-container");
+  listContainer.appendChild(todoContainer);
+  list.todoArray.forEach((element) => {
+    // Todo box
+    const todo = document.createElement("div");
+    const todoIdName = `${element.id}`;
+    todo.classList.add("todo");
+    todo.setAttribute("id", todoIdName);
+    todoContainer.appendChild(todo);
+    // Checkbox
+    const checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.setAttribute("id", `chk-${todoIdName}`);
+    todo.appendChild(checkBox);
+    // Todo name
+    const todoName = document.createElement(todoNameElement);
+    todoName.classList.add("todo-name");
+    todoName.textContent = element.id;
+    todo.appendChild(todoName);
+    // Todo icons
+    const todoIcons = document.createElement("div");
+    todoIcons.classList.add("icon-container");
+    const todoEdit = new Image();
+    todoEdit.src = fileEditIcon;
+    todoEdit.classList.add("edit-icon");
+    todoEdit.setAttribute("id", `edit-${todoIdName}`);
+    todoEdit.setAttribute("type", "todo");
+    todoEdit.setAttribute("listId", listIdName);
+    todoIcons.appendChild(todoEdit);
+    const todoDel = new Image();
+    todoDel.src = trashCanIcon;
+    todoDel.classList.add("del-icon");
+    todoDel.setAttribute("id", `del-${todoIdName}`);
+    todoDel.setAttribute("type", "todo");
+    todoDel.setAttribute("listId", listIdName);
+    todoIcons.appendChild(todoDel);
+    todo.appendChild(todoIcons);
+    // Add todo to container
+    todoContainer.appendChild(todo);
+  });
+
+  // Add list container to DOM
+  projectContainer.appendChild(listContainer);
+};
+
+/*
+  ___UPDATE PROJECT VIEW___
+*/
+// Function to remove list from DOM
+function removeListFromDOM(list) {
+  // Get project element from DOM
+  const proj = list.parentElement;
+  // Remove the list DOM element from the project DOM element
+  proj.removeChild(list);
+}
+
+// Function to add list to DOM
+function addListToDOM(list) {
+  // Display list in DOM
+  displayList(list);
+}
+
+// Function to update project view
+function updateProjView(listId, array) {
+  if (array.length === 0) {
+    // If array empty
+    // Get DOM element for list
+    const listDOM = document.getElementById(listId);
+    // Remove list DOM element from project DOM element
+    removeListFromDOM(listDOM);
+  } else {
+    // For each element in the list array
+    array.forEach((element) => {
+      // Get old DOM element for this array element
+      const oldDOM = document.getElementById(element.id);
+      // Remove old DOM element
+      removeListFromDOM(oldDOM);
+      // Add new DOM element
+      addListToDOM(element);
+    });
+  }
+}
+
+/*
+  ___DISPLAY PROJECT___
+*/
+function displayProject(projObj) {
+  // Get the project ID
+  const projectID = projObj.id;
+  // Get the project container element from DOM
+  const projectContainer = document.querySelector(".project-container");
+  // Add ID to project container element
+  projectContainer.setAttribute("id", projectID);
+
+  projObj.listArray.forEach((element) => {
+    // console.log(element);
+    displayList(element);
+  });
+}
+
+export { setUpDOM, updateProjView, displayProject };
