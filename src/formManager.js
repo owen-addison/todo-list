0848a184ed58aeea151d7537f161ef648540639d
Owen Lewis <owenalewis@gmail.com>
2023-03-21 08:54:17 +0000

    Creates formManager.js and adds function for todo form

---
+++ src/formManager.js
@@ -0,0 +1,14 @@
+// Todo edit form
+function generateTodoForm() {
+  // Create form background
+  const formBackground = document.createElement("div");
+  formBackground.classList.add("form-background");
+  // Create form container
+  const formContainer = document.createElement("div");
+  formContainer.classList.add("form-container");
+  formBackground.appendChild(formContainer);
+}
+
+// List edit form
+
+// Project edit form
