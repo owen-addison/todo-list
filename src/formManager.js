9681e02c86f3705837e5ecb78ec9212492621f90
Owen Lewis <owenalewis@gmail.com>
2023-03-21 11:40:28 +0000

    Defines first elements in todo form section Creates array for options of priority selection. Adds and defines containers and elements for name and priority of todo item.

--- src/formManager.js
+++ src/formManager.js
@@ -1,3 +1,6 @@
+// Define priority options array
+const priorityOptionsArray = ["none", "low", "medium", "high"];
+
 // Todo edit form
 function generateTodoForm() {
   // Create form background
@@ -6,7 +9,56 @@ function generateTodoForm() {
   // Create form container
   const formContainer = document.createElement("div");
   formContainer.classList.add("form-container");
+
+  // Append container to background element
   formBackground.appendChild(formContainer);
+
+  // Add container for name elements
+  const nameDiv = document.createElement("div");
+  nameDiv.classList.add("form-div");
+  // Add label for name input
+  const todoNameLabel = document.createElement("label");
+  todoNameLabel.setAttribute("for", "todo-name");
+  // Add input for name and set attributes
+  const todoNameInput = document.createElement("input");
+  todoNameInput.setAttribute("type", "text");
+  todoNameInput.setAttribute("id", "todo-name");
+  todoNameInput.setAttribute("name", "todo-name");
+
+  // Append both label and input for name to name container
+  nameDiv.appendChild(todoNameLabel);
+  nameDiv.appendChild(todoNameInput);
+  // Append the name container to form container
+  formContainer.appendChild(nameDiv);
+
+  // Add container for priority elements
+  const priorityDiv = document.createElement("div");
+  priorityDiv.classList.add("form-div");
+  // Add label for priority select
+  const priorityLabel = document.createElement("label");
+  priorityLabel.setAttribute("for", "todo-priority");
+  // Add priority select element
+  const prioritySelect = document.createElement("select");
+  prioritySelect.name = "todo-priority";
+  // Loop through options array
+  priorityOptionsArray.forEach((element) => {
+    // Create option element
+    const priorityOption = document.createElement("option");
+    // Set option value to the string of each array item
+    priorityOption.setAttribute("value", element);
+    // Get string for option name
+    const optionName = element;
+    // Get the first letter of the name
+    const firstLetter = optionName.charAt(0);
+    // Set the text to name with capital first letter
+    priorityOption.innerText = firstLetter.toUpperCase() + optionName.slice(1);
+    // Add option element to select element
+    prioritySelect.appendChild(priorityOption);
+  });
+  // Add the select to priority container
+  priorityDiv.appendChild(prioritySelect);
+  // Append the priority container to the form container
+  formContainer.appendChild(priorityDiv);
 }
 
 // List edit form
