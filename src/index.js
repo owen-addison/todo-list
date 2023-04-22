d32d6e7390945c9633755154ab6c95744c8e3d01
Owen Lewis <owenalewis@gmail.com>
2023-04-22 15:44:08 +0100

    Removes unrequired commented out code block

--- src/index.js
+++ src/index.js
@@ -35,28 +35,6 @@ const createProj = (name = "New Project", info = undefined) => {
 };
 
 /*
-  ___OBJECT LOGIC___
-*/
-// // Get object from array
-// function returnObjectFromArray(id, array) {
-//   // Filter through array of lists to find matching list object
-//   const filteredArray = array.filter((element) => element.id === id);
-//   // Check for uniqueness
-//   if (filteredArray.length > 1) {
-//     // If more than one list object found throw error
-//     console.log(filteredArray);
-//     throw Error("ERROR! More than one relative found."); // Error if more that one relative object found
-//   } else if (filteredArray.length < 1) {
-//     throw Error("ERROR! No relative found."); // Error if no relative object found
-//   } else {
-//     // Destructure array of objects
-//     const [relative] = filteredArray;
-//     // Return list object
-//     return relative;
-//   }
-// }
-
-/*
   ___EVENT HANDLERS___ (for event listeners)
 */
 // Handle todo add event
